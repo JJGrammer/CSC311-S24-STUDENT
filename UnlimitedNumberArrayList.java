299c32aceebd80916188355b9d485cc0dd142c5c
JJGrammer <jaramillo.johann2@gmail.com>
2024-02-16 20:14:13 -0800

    Finished debugging NumberArrayList.java and fixed all errors, file is fully functional. Added UnlimitedNumberArrayList.java, a copy of NumberArrayList.java with the ability to expand size upon adding an element to a full array.

--- UnlimitedNumberArrayList.java
+++ UnlimitedNumberArrayList.java
@@ -1,11 +1,11 @@
 import java.util.Arrays;
 
 //Johann Jaramillo (jjaramillo26@toromail.csudh.edu)
-public class NumberArrayList {
+public class UnlimitedNumberArrayList {
     int [] myArray;
 
-    public NumberArrayList(int size){
-         myArray = new int[size];    //init our array
+    public UnlimitedNumberArrayList(int size){
+        myArray = new int[size];    //init our array
     }
     public int get(int index){
         return myArray[index];  //return value
@@ -13,16 +13,26 @@ public class NumberArrayList {
 
     public int add(int newNum){ //add a new value to array and return its index
         int indexOfNew=0;
-
+        arrayProcess:
         for(int index=0;index< myArray.length;index++){
 
-           if(myArray[index]==0){
-               myArray[index] = newNum;
-               indexOfNew= index;
-               break ;
-           }
+            if(myArray[index]==0){
+                myArray[index] = newNum;
+                indexOfNew= index;
+               // break arrayProcess;
+            }
+            else{
+                int[] tempArr = new int[myArray.length+1];
+
+                for(int x=0; x<myArray.length;x++){
+                    tempArr[x] = myArray[x];
+                }
+                tempArr[myArray.length] = newNum;
+                indexOfNew = tempArr.length;
+                myArray = tempArr.clone();
+            }
         }
-       return indexOfNew;
+        return indexOfNew;
     }
 
     public int size(){ //return num of elems in my array
@@ -61,10 +71,10 @@ public class NumberArrayList {
         int smallestNum;
         if(myArray.length!=0){
             smallestNum = myArray[0];
-        for(int ind: myArray){
+            for(int ind: myArray){
                 if(ind<smallestNum){
                     smallestNum = ind;
-            } }
+                } }
             return smallestNum;
         }
         return -1 ;
@@ -76,11 +86,12 @@ public class NumberArrayList {
             total += ind;
         }
         if (myArray.length!=0){
-        return (int)total/(myArray.length); }
+            return (int)total/(myArray.length); }
         else {return -1;}
     }
     @Override
     public String toString() {
+
         return Arrays.toString(myArray);
     }
 }
